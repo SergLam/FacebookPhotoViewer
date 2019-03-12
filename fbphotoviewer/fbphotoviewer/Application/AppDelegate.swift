6e1ad23b740b676d4c66caa5794a445c39567b73
Sergey Lamcev <sergey.lamcev@anoda.mobi>
2019-03-12 09:53:28 +0200

    Facebook SDK setup

---
+++ fbphotoviewer/fbphotoviewer/Application/AppDelegate.swift
@@ -0,0 +1,51 @@
+//
+//  AppDelegate.swift
+//  fbphotoviewer
+//
+//  Created by Serg Liamthev on 3/11/19.
+//  Copyright © 2019 serglam. All rights reserved.
+//
+
+import UIKit
+import FacebookCore
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Facebook setup
+        SDKApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
+        
+        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .white
+        window?.makeKeyAndVisible()
+        let rootVC = ViewController()
+        window?.rootViewController = rootVC
+        return true
+    }
+    
+    // MARK: Facebook methods
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return SDKApplicationDelegate.shared.application(application,
+                                                         open: url,
+                                                         sourceApplication: sourceApplication,
+                                                         annotation: annotation)
+    }
+    
+    @available(iOS 9.0, *)
+    func application(_ application: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any]) -> Bool {
+        return SDKApplicationDelegate.shared.application(application, open: url, options: options)
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        AppEventsLogger.activate(application)
+    }
+
+}
+
