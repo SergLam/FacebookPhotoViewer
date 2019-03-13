518c4d450c72b379f3acc1c732418f9ced73949d
Sergey Lamcev <sergey.lamcev@anoda.mobi>
2019-03-14 00:27:28 +0200

    Added datamodels files

---
+++ fbphotoviewer/fbphotoviewer/DataModels/FBUser.swift
@@ -0,0 +1,13 @@
+//
+//  FBUser.swift
+//  fbphotoviewer
+//
+//  Created by Serg Liamthev on 3/14/19.
+//  Copyright © 2019 serglam. All rights reserved.
+//
+
+import Foundation
+
+class FBUser {
+    
+}
